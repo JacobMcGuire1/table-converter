95682e43f2bcb094aa48db4bcc414604fd29ef05
JacobMcGuire1 <mcguirejacob1@gmail.com>
2020-11-02 10:57:22 +0000

    Data can now be entered into table and table can convert to latex

--- Table-Creator/ClientApp/src/components/MyTable.js
+++ Table-Creator/ClientApp/src/components/MyTable.js
@@ -19,6 +19,7 @@ var MyTable = /** @class */ (function (_super) {
     __extends(MyTable, _super);
     function MyTable(props) {
         var _this = _super.call(this, props) || this;
+        _this.init = false;
         _this.addRow = _this.addRow.bind(_this);
         _this.addCol = _this.addCol.bind(_this);
         _this.state = { table: [], rows: 5, cols: 5 };
@@ -30,24 +31,103 @@ var MyTable = /** @class */ (function (_super) {
         for (var i = 0; i < this.state.rows; i++) {
             var row = [];
             for (var j = 0; j < this.state.cols; j++) {
-                row.push(React.createElement(Cell, { data: (i * j).toString() }));
+                row.push((i * j).toString());
             }
             this.state.table.push(row);
         }
+        this.init = true;
         //this.table = newtable;
     };
     MyTable.prototype.addRow = function () {
-        this.setState({ table: [] });
         this.setState({ rows: this.state.rows + 1 });
+        var newtable = this.state.table.map(function (x) { return x; });
+        var row = [];
+        for (var j = 0; j < this.state.cols; j++) {
+            row.push("");
+        }
+        newtable.push(row);
+        this.setState({ table: newtable });
     };
     MyTable.prototype.addCol = function () {
-        this.setState({ table: [] });
         this.setState({ cols: this.state.cols + 1 });
+        //this.setState({ table: [] });
+        var newtable = this.state.table.map(function (x) { return x; });
+        for (var i = 0; i < this.state.rows; i++) {
+            newtable[i].push("");
+        }
+        this.setState({ table: newtable });
+    };
+    MyTable.prototype.modifyCellData = function (x, y, data) {
+        var newtable = this.state.table.map(function (x) { return x; });
+        newtable[x][y] = data;
+        this.setState({ table: newtable });
+    };
+    MyTable.prototype.convertToLatex = function () {
+        var collatex = "|";
+        for (var i = 0; i < this.state.cols; i++) {
+            collatex = collatex + "c|";
+        }
+        var latextable = [];
+        var _loop_1 = function (i) {
+            var row = this_1.state.table[i];
+            var rowlatex = "";
+            row.forEach(function (x) { return rowlatex = rowlatex + x + " & "; });
+            rowlatex = rowlatex.slice(0, -3);
+            rowlatex = rowlatex + " \\\\";
+            latextable.push(rowlatex);
+        };
+        var this_1 = this;
+        for (var i = 0; i < this.state.rows; i++) {
+            _loop_1(i);
+        }
+        var bs = "\\";
+        var cu1 = "{";
+        var cu2 = "}";
+        return (React.createElement("div", null,
+            bs,
+            "begin",
+            cu1,
+            "center",
+            cu2,
+            React.createElement("br", null),
+            bs,
+            "begin",
+            cu1,
+            "tabular",
+            cu2,
+            cu1,
+            collatex,
+            cu2,
+            React.createElement("br", null),
+            latextable.map(function (x) { return React.createElement("div", null, x); }),
+            bs,
+            "end",
+            cu1,
+            "tabular",
+            cu2,
+            React.createElement("br", null),
+            bs,
+            "end",
+            cu1,
+            "center",
+            cu2));
     };
     MyTable.prototype.drawTable = function () {
-        this.testPopulateTable();
-        return (React.createElement("table", null,
-            React.createElement("tbody", null, this.state.table.map(function (innerArray, i) { return (React.createElement("tr", { key: i }, innerArray.map(function (item, j) { return React.createElement("td", { key: (i + 1) * (j + 1) }, item); }))); }))));
+        //alert("Drawing table!");
+        var _this = this;
+        if (!this.init) {
+            this.testPopulateTable();
+        }
+        return (React.createElement("div", null,
+            React.createElement("table", null,
+                React.createElement("tbody", null, this.state.table.map(function (innerArray, i) { return (React.createElement("tr", { key: i }, innerArray.map(function (item, j) {
+                    return React.createElement("td", { key: i + "," + j },
+                        React.createElement(Cell, { data: item, x: i, y: j, changeData: function (x, y, data) { return _this.modifyCellData(x, y, data); } }));
+                }))); }))),
+            React.createElement("br", null),
+            "LaTeX:",
+            React.createElement("br", null),
+            this.convertToLatex()));
     };
     MyTable.prototype.render = function () {
         return (React.createElement("div", null,
@@ -59,15 +139,23 @@ var MyTable = /** @class */ (function (_super) {
     return MyTable;
 }(React.Component));
 exports.default = MyTable;
+/*type CellState = {
+    data: string
+}*/
 var Cell = /** @class */ (function (_super) {
     __extends(Cell, _super);
     function Cell(props) {
         var _this = _super.call(this, props) || this;
-        _this.data = _this.props.data;
+        _this.state = { data: _this.props.data };
         return _this;
     }
+    Cell.prototype.changeData = function (e) {
+        this.props.changeData(this.props.x, this.props.y, e.target.value);
+    };
     Cell.prototype.render = function () {
-        return (React.createElement("div", null, this.data));
+        var _this = this;
+        return (React.createElement("div", null,
+            React.createElement("textarea", { onChange: function (e) { return _this.changeData(e); }, value: this.props.data })));
     };
     return Cell;
 }(React.Component));
--- Table-Creator/ClientApp/src/components/MyTable.tsx
+++ Table-Creator/ClientApp/src/components/MyTable.tsx
@@ -6,13 +6,13 @@ type Props = {
 }
 
 type TableState = {
-    table: Cell[][];
+    table: string[][];
     rows: number;
     cols: number;
 }
 
 class MyTable extends React.Component<Props, TableState> {
-    
+    private init = false
     constructor(props: Props) {
         super(props);
         this.addRow = this.addRow.bind(this);
@@ -23,34 +23,106 @@ class MyTable extends React.Component<Props, TableState> {
     }
     private testPopulateTable() {
         for (let i = 0; i < this.state.rows; i++) {
-            let row:Cell[] = [];
+            let row:string[] = [];
             for (let j = 0; j < this.state.cols; j++) {
-                row.push(<Cell data={(i * j).toString()} /> as unknown as Cell);
+                row.push((i * j).toString());
             }
             this.state.table.push(row)
         }
+        this.init = true;
         //this.table = newtable;
     }
     private addRow() {
-        this.setState({ table: [] });
         this.setState({ rows: this.state.rows + 1 });
+
+        let newtable = this.state.table.map((x) => x);
+        let row: string[] = [];
+        for (let j = 0; j < this.state.cols; j++) {
+            row.push("");
+        }
+        newtable.push(row);
+        this.setState({ table: newtable });
     }
     private addCol() {
-        this.setState({ table: [] });
-        this.setState({ cols: this.state.cols + 1 });
+        this.setState({ cols: this.state.cols + 1 })
+        //this.setState({ table: [] });
+        let newtable = this.state.table.map((x) => x);
+        for (let i = 0; i < this.state.rows; i++) {
+            newtable[i].push("");
+        }
+        this.setState({ table: newtable });
+    }
+    private modifyCellData(x: number, y: number, data: string) {
+        let newtable = this.state.table.map((x) => x);
+        newtable[x][y] = data;
+        this.setState({ table: newtable });
+    }
+    private convertToLatex() {
+        
+
+        let collatex = "|";
+        for (let i = 0; i < this.state.cols; i++) {
+            collatex = collatex + "c|";
+        }
+
+        let latextable = [];
+        for (let i = 0; i < this.state.rows; i++) {
+            let row = this.state.table[i];
+            let rowlatex = ""
+            row.forEach((x) => rowlatex = rowlatex + x + " & ")
+            rowlatex = rowlatex.slice(0, -3);
+            rowlatex = rowlatex + " \\\\";
+            latextable.push(rowlatex);
+        }
+
+        let bs = "\\";
+        let cu1 = "{";
+        let cu2 = "}";
+
+        return (
+            <div>
+                    {bs}begin{cu1}center{cu2}
+                    <br/>
+                    {bs}begin{cu1}tabular{cu2}{cu1}{collatex}{cu2}
+                    <br />
+                    {latextable.map((x) => <div>{x}</div>)}
+                    {bs}end{cu1}tabular{cu2}
+                    <br/>
+                    {bs}end{cu1}center{cu2}
+            </div>
+            );
     }
     private drawTable() {
-        this.testPopulateTable();
+        //alert("Drawing table!");
+        
+        if (!this.init) {
+            this.testPopulateTable();
+        }
+        
         return (
-            <table>
-                <tbody>
-                    {this.state.table.map((innerArray, i) => (
-                        <tr key={i}>
-                            {innerArray.map((item, j) => <td key={(i + 1) * (j + 1)}>{item}</td>)}
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
+            <div>
+                <table>
+                    <tbody>
+                        {this.state.table.map((innerArray, i) => (
+                            <tr key={i}>
+                                {
+                                    innerArray.map(
+                                        (item, j) =>
+                                            <td key={i + "," + j}>
+                                                <Cell data={item} x={i} y={j} changeData={(x: number, y: number, data: string) => this.modifyCellData(x, y, data)} />
+                                            </td>
+                                    )
+                                }
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+
+                <br />
+                LaTeX:
+                <br/>
+                {this.convertToLatex()}
+            </div>
             );
     }
     public render() {
@@ -69,19 +141,28 @@ export default MyTable;
 
 interface CellProps {
     data: string
+    x: number
+    y: number
+    changeData: Function
 }
+
+/*type CellState = {
+    data: string
+}*/
 
 
 class Cell extends React.Component<CellProps, {}> {
-    private data: string;
     constructor(props: CellProps) {
         super(props);
-        this.data = this.props.data
+        this.state = { data: this.props.data };
+    }
+    private changeData(e: React.ChangeEvent<HTMLTextAreaElement>) {
+        this.props.changeData(this.props.x, this.props.y, e.target.value);
     }
     public render() {
         return (
             <div>
-                { this.data }
+                <textarea onChange={(e) => this.changeData(e)} value={this.props.data}/>
             </div>
         );
     }
