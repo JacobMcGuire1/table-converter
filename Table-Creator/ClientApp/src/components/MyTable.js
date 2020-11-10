8514e330b11c5108b081523a0006564aef26e3e5
JacobMcGuire1 <mcguirejacob1@gmail.com>
2020-11-10 17:03:36 +0000

    Switched to SVG format for table, display and table adjusts to content size.

--- Table-Creator/ClientApp/src/components/MyTable.js
+++ Table-Creator/ClientApp/src/components/MyTable.js
@@ -26,7 +26,7 @@ var MyTable = /** @class */ (function (_super) {
         var _this = _super.call(this, props) || this;
         _this.addRow = _this.addRow.bind(_this);
         _this.addCol = _this.addCol.bind(_this);
-        _this.state = { table: [], rows: 5, cols: 5 };
+        _this.state = { table: [], rows: 5, cols: 5, mincellheight: 50, mincellwidth: 150, dividerpixels: 5, colwidths: [], rowheights: [] };
         _this.testPopulateTable();
         return _this;
     }
@@ -37,6 +37,12 @@ var MyTable = /** @class */ (function (_super) {
                 row.push((i * j).toString());
             }
             this.state.table.push(row);
+        }
+        for (var i = 0; i < this.state.rows; i++) { //Must change this if we initialise table with data.
+            this.state.rowheights.push(this.state.mincellheight);
+        }
+        for (var i = 0; i < this.state.cols; i++) { //Must change this if we initialise table with data.
+            this.state.colwidths.push(this.state.mincellwidth);
         }
     };
     MyTable.prototype.addRow = function () {
@@ -60,7 +66,69 @@ var MyTable = /** @class */ (function (_super) {
     MyTable.prototype.modifyCellData = function (x, y, data) {
         var newtable = this.state.table.map(function (x) { return x; });
         newtable[x][y] = data;
-        this.setState({ table: newtable });
+        var lines = data.split("\n");
+        var largestwidth = 0;
+        for (var i = 0; i < lines.length; i++) {
+            //document.measureText(lines[i]);
+            var width = this.checkTextSize(data).width * 1.75;
+            if (width > largestwidth) {
+                largestwidth = width;
+            }
+        }
+        ;
+        if (largestwidth > this.state.colwidths[y]) {
+            var newcolwidths = this.state.colwidths.map(function (x) { return x; });
+            newcolwidths[y] = largestwidth;
+            this.setState({ table: newtable, colwidths: newcolwidths });
+            //console.log(this.state.colwidths.toString());
+        }
+        else {
+            if (largestwidth < this.state.colwidths[y]) {
+                var newcolwidth = this.adjustColWidth(y);
+                if (newcolwidth !== -1) {
+                    var newcolwidths = this.state.colwidths.map(function (x) { return x; });
+                    if (newcolwidth > this.state.mincellwidth) {
+                        newcolwidths[y] = newcolwidth;
+                    }
+                    else {
+                        newcolwidths[y] = this.state.mincellwidth;
+                    }
+                    this.setState({ table: newtable, colwidths: newcolwidths });
+                }
+                else {
+                    this.setState({ table: newtable });
+                }
+            }
+            else {
+                this.setState({ table: newtable });
+            }
+        }
+    };
+    MyTable.prototype.adjustColWidth = function (col) {
+        var currentwidth = this.state.colwidths[col];
+        var largestwidth = 0;
+        for (var i = 0; i < this.state.rows; i++) {
+            var data = this.state.table[i][col];
+            var lines = data.split("\n");
+            for (var j = 0; j < lines.length; j++) {
+                var width = this.checkTextSize(data).width * 1.75;
+                if (width > largestwidth) {
+                    largestwidth = width;
+                }
+            }
+        }
+        if (largestwidth < currentwidth) {
+            return largestwidth;
+        }
+        else {
+            return -1;
+        }
+    };
+    MyTable.prototype.checkTextSize = function (text) {
+        var canvas = document.createElement('canvas'), context = canvas.getContext('2d');
+        return context === null || context === void 0 ? void 0 : context.measureText(text);
+    };
+    MyTable.prototype.resizeRow = function (width, height) {
     };
     MyTable.prototype.convertToLatex = function () {
         var collatex = "|";
@@ -75,7 +143,6 @@ var MyTable = /** @class */ (function (_super) {
             rowlatex = rowlatex.slice(0, -3);
             rowlatex = rowlatex + " \\\\";
             latextable.push(rowlatex);
-            console.log(rowlatex);
         };
         var this_1 = this;
         for (var i = 0; i < this.state.rows; i++) {
@@ -100,7 +167,7 @@ var MyTable = /** @class */ (function (_super) {
             collatex,
             cu2,
             React.createElement("br", null),
-            latextable.map(function (x) { return React.createElement("div", null, x); }),
+            latextable.map(function (x) { return React.createElement("div", { key: x }, x); }),
             bs,
             "end",
             cu1,
@@ -116,6 +183,9 @@ var MyTable = /** @class */ (function (_super) {
     MyTable.prototype.drawTable = function () {
         var _this = this;
         return (React.createElement("div", null,
+            React.createElement("svg", { width: this.state.colwidths.reduce(function (a, b) { return a + b; }, 0) + (this.state.dividerpixels * this.state.cols), height: this.state.rowheights.reduce(function (a, b) { return a + b; }, 0) + (this.state.dividerpixels * this.state.rows), id: "svg" }, this.state.table.map(function (innerArray, x) { return (innerArray.map(function (item, y) {
+                return React.createElement(SVGCell, { data: item, x: x, y: y, xpixel: (_this.state.colwidths.slice(0, y)).reduce(function (a, b) { return a + b; }, 0) + (_this.state.dividerpixels * (y)) /*xpixel={y * (this.state.mincellwidth + 10) /*Need to make these 2 count the heights/widths.*/, ypixel: x * (_this.state.mincellheight + _this.state.dividerpixels), width: _this.state.colwidths[y], height: _this.state.rowheights[x], changeData: function (x, y, data) { return _this.modifyCellData(x, y, data); }, resizeRow: function (width, height) { return _this.resizeRow(width, height); } });
+            })); })),
             React.createElement("table", null,
                 React.createElement("tbody", null, this.state.table.map(function (innerArray, i) { return (React.createElement("tr", { key: i }, innerArray.map(function (item, j) {
                     return React.createElement("td", { key: i + "," + j },
@@ -136,9 +206,6 @@ var MyTable = /** @class */ (function (_super) {
     return MyTable;
 }(React.Component));
 exports.default = MyTable;
-/*type CellState = {
-    data: string
-}*/
 var Cell = /** @class */ (function (_super) {
     __extends(Cell, _super);
     function Cell(props) {
@@ -155,5 +222,55 @@ var Cell = /** @class */ (function (_super) {
             React.createElement("textarea", { onChange: function (e) { return _this.changeData(e); }, value: this.props.data })));
     };
     return Cell;
+}(React.Component));
+var SVGCell = /** @class */ (function (_super) {
+    __extends(SVGCell, _super);
+    function SVGCell(props) {
+        var _this = _super.call(this, props) || this;
+        _this.state = { editing: false };
+        _this.ref = React.createRef();
+        return _this;
+        //this.state = { data: this.props.data };
+    }
+    SVGCell.prototype.toggleEditMode = function () {
+        this.setState({ editing: !this.state.editing });
+    };
+    SVGCell.prototype.changeData = function (e) {
+        this.props.changeData(this.props.x, this.props.y, e.target.value);
+    };
+    SVGCell.prototype.moveCursorToEnd = function (e) {
+        var value = e.target.value;
+        e.target.value = "";
+        e.target.value = value;
+    };
+    SVGCell.prototype.getText = function () {
+        var _this = this;
+        if (!this.state.editing) {
+            return (React.createElement("text", { x: this.props.xpixel + this.props.width / 2, y: this.props.ypixel + 20, textAnchor: "middle", alignmentBaseline: "central" /* Should let user choose alignment*/ }, this.props.data));
+        }
+        else {
+            return (React.createElement("foreignObject", { x: this.props.xpixel, y: this.props.ypixel, width: this.props.width, height: this.props.height },
+                React.createElement("div", null,
+                    React.createElement("textarea", { onChange: function (e) { return _this.changeData(e); }, value: this.props.data, autoFocus: true, onBlur: function () { return _this.toggleEditMode(); }, onFocus: function (e) { return _this.moveCursorToEnd(e); } }))));
+        }
+    };
+    SVGCell.prototype.componentDidUpdate = function () {
+        var k = this.ref;
+        var j = k.current;
+        if (!this.state.editing) {
+            //console.log(this.props.x.toString() + this.props.y.toString() + "dwad" + text.scrollWidth);
+            var text = j.children[1];
+            //text.
+        }
+        //console.log(text.clientWidth);
+        //if (text is )
+    };
+    SVGCell.prototype.render = function () {
+        var _this = this;
+        return (React.createElement("g", { onDoubleClick: function () { return _this.toggleEditMode(); }, id: "cell:" + this.props.xpixel.toString() + this.props.ypixel.toString(), ref: this.ref },
+            React.createElement("rect", { x: this.props.xpixel, y: this.props.ypixel, width: this.props.width, height: this.props.height, fill: "grey" }),
+            this.getText()));
+    };
+    return SVGCell;
 }(React.Component));
 //# sourceMappingURL=MyTable.js.map
--- Table-Creator/ClientApp/src/components/MyTable.tsx
+++ Table-Creator/ClientApp/src/components/MyTable.tsx
@@ -9,6 +9,11 @@ type TableState = {
     table: string[][];
     rows: number;
     cols: number;
+    mincellheight: number;
+    mincellwidth: number;
+    dividerpixels: number;
+    colwidths: number[];
+    rowheights: number[];
 }
 
 function escapeLatex(str: string){
@@ -22,7 +27,7 @@ class MyTable extends React.Component<Props, TableState> {
         super(props);
         this.addRow = this.addRow.bind(this);
         this.addCol = this.addCol.bind(this);
-        this.state = { table: [], rows: 5, cols: 5 };
+        this.state = { table: [], rows: 5, cols: 5, mincellheight: 50, mincellwidth: 150, dividerpixels: 5, colwidths: [], rowheights: []};
         this.testPopulateTable();
     }
     private testPopulateTable() {
@@ -32,6 +37,12 @@ class MyTable extends React.Component<Props, TableState> {
                 row.push((i * j).toString());
             }
             this.state.table.push(row)
+        }
+        for (let i = 0; i < this.state.rows; i++) { //Must change this if we initialise table with data.
+            this.state.rowheights.push(this.state.mincellheight);
+        }
+        for (let i = 0; i < this.state.cols; i++) { //Must change this if we initialise table with data.
+            this.state.colwidths.push(this.state.mincellwidth);
         }
     }
     private addRow() {
@@ -55,7 +66,67 @@ class MyTable extends React.Component<Props, TableState> {
     private modifyCellData(x: number, y: number, data: string) {
         let newtable = this.state.table.map((x) => x);
         newtable[x][y] = data;
-        this.setState({ table: newtable });
+
+        let lines = data.split("\n");
+        let largestwidth = 0;
+        for (let i = 0; i < lines.length; i++) {
+            //document.measureText(lines[i]);
+            let width = this.checkTextSize(data)!.width * 1.75;
+            if (width > largestwidth) {
+                largestwidth = width;
+            }
+        }
+;
+        if (largestwidth > this.state.colwidths[y]) {
+            let newcolwidths = this.state.colwidths.map((x) => x);
+            newcolwidths[y] = largestwidth;
+            this.setState({ table: newtable, colwidths: newcolwidths });
+            //console.log(this.state.colwidths.toString());
+        } else {
+            if (largestwidth < this.state.colwidths[y]) {
+                let newcolwidth = this.adjustColWidth(y);
+                if (newcolwidth !== -1) {
+                    let newcolwidths = this.state.colwidths.map((x) => x);
+                    if (newcolwidth > this.state.mincellwidth) {
+                        newcolwidths[y] = newcolwidth;
+                    } else {
+                        newcolwidths[y] = this.state.mincellwidth;
+                    }
+                    this.setState({ table: newtable, colwidths: newcolwidths });
+                } else {
+                    this.setState({ table: newtable });
+                }
+            } else {
+                this.setState({ table: newtable });
+            }
+        } 
+    }
+    private adjustColWidth(col: number) {
+        let currentwidth = this.state.colwidths[col];
+        let largestwidth = 0;
+        for (let i = 0; i < this.state.rows; i++) {
+            let data = this.state.table[i][col];
+            let lines = data.split("\n");
+            for (let j = 0; j < lines.length; j++) {
+                let width = this.checkTextSize(data)!.width * 1.75;
+                if (width > largestwidth) {
+                    largestwidth = width;
+                }
+            }
+        }
+        if (largestwidth < currentwidth) {
+            return largestwidth;
+        } else {
+            return -1;
+        }
+    }
+    private checkTextSize(text: string) {
+        var canvas = document.createElement('canvas'),
+            context = canvas.getContext('2d');
+        return context?.measureText(text);
+    }
+    private resizeRow(width: number, height: number) {
+
     }
     private convertToLatex() {
         
@@ -73,7 +144,6 @@ class MyTable extends React.Component<Props, TableState> {
             rowlatex = rowlatex.slice(0, -3);
             rowlatex = rowlatex + " \\\\";
             latextable.push(rowlatex);
-            console.log(rowlatex);
         }
         
         let bs = "\\";
@@ -85,8 +155,8 @@ class MyTable extends React.Component<Props, TableState> {
                     {bs}begin{cu1}center{cu2}
                     <br/>
                     {bs}begin{cu1}tabular{cu2}{cu1}{collatex}{cu2}
-                    <br />
-                    {latextable.map((x) => <div>{x}</div>)}
+                    <br/>
+                    {latextable.map((x) => <div key={x}>{x}</div>)}
                     {bs}end{cu1}tabular{cu2}
                     <br/>
                     {bs}end{cu1}center{cu2}
@@ -96,6 +166,25 @@ class MyTable extends React.Component<Props, TableState> {
     private drawTable() {        
         return (
             <div>
+                <svg width={this.state.colwidths.reduce((a, b) => a + b, 0) + (this.state.dividerpixels * this.state.cols)} height={this.state.rowheights.reduce((a, b) => a + b, 0) + (this.state.dividerpixels * this.state.rows)} id="svg">
+                    {this.state.table.map((innerArray, x) => (
+                        innerArray.map(
+                            (item, y) =>
+                                <SVGCell
+                                    data={item}
+                                    x={x}
+                                    y={y}
+                                    xpixel={(this.state.colwidths.slice(0, y)).reduce((a, b) => a + b, 0) + (this.state.dividerpixels * (y)) /*xpixel={y * (this.state.mincellwidth + 10) /*Need to make these 2 count the heights/widths.*/}
+                                    ypixel={x * (this.state.mincellheight + this.state.dividerpixels)}
+                                    width={this.state.colwidths[y]}
+                                    height={this.state.rowheights[x]}
+                                    changeData={(x: number, y: number, data: string) => this.modifyCellData(x, y, data)}
+                                    resizeRow={(width: number, height: number) => this.resizeRow(width, height)}
+                                />
+                        )
+                    ))}
+                </svg>
+
                 <table>
                     <tbody>
                         {this.state.table.map((innerArray, i) => (
@@ -143,11 +232,6 @@ interface CellProps {
     changeData: Function
 }
 
-/*type CellState = {
-    data: string
-}*/
-
-
 class Cell extends React.Component<CellProps, {}> {
     constructor(props: CellProps) {
         super(props);
@@ -161,6 +245,81 @@ class Cell extends React.Component<CellProps, {}> {
             <div>
                 <textarea onChange={(e) => this.changeData(e)} value={this.props.data}/>
             </div>
+        );
+    }
+}
+
+//Style stuff should be here too.
+interface SVGCellProps {
+    data: string
+    x: number
+    y: number
+    xpixel: number
+    ypixel: number
+    width: number
+    height: number
+    changeData: Function
+    resizeRow: Function
+}
+
+interface SVGCellState {
+    editing: boolean
+}
+
+class SVGCell extends React.Component<SVGCellProps, SVGCellState> {
+    private ref: React.RefObject<SVGGElement>;
+    constructor(props: SVGCellProps) {
+        super(props);
+        this.state = { editing: false }
+        this.ref = React.createRef()
+        //this.state = { data: this.props.data };
+    }
+    private toggleEditMode() {
+        this.setState({ editing: !this.state.editing });
+    }
+    private changeData(e: React.ChangeEvent<HTMLTextAreaElement>) {
+        this.props.changeData(this.props.x, this.props.y, e.target.value);
+    }
+    private moveCursorToEnd(e: React.FocusEvent<HTMLTextAreaElement>) {
+        let value = e.target.value;
+        e.target.value = "";
+        e.target.value = value;
+    }
+
+    private getText() {
+        if (!this.state.editing) {
+            return (
+                <text x={this.props.xpixel + this.props.width / 2} y={this.props.ypixel + 20} textAnchor={"middle" } alignmentBaseline={"central"/* Should let user choose alignment*/}>{this.props.data}</text>
+            );
+        } else {
+            return (
+                <foreignObject x={this.props.xpixel} y={this.props.ypixel} width={this.props.width} height={this.props.height}>
+                    <div>
+                        <textarea onChange={(e) => this.changeData(e)} value={this.props.data} autoFocus={true} onBlur={() => this.toggleEditMode()} onFocus={(e) => this.moveCursorToEnd(e)} />
+                    </div>
+                </foreignObject>
+            );
+        }
+    }
+    componentDidUpdate() {
+        let k = this.ref!;
+        let j = k.current!;
+        
+        if (!this.state.editing) {
+            //console.log(this.props.x.toString() + this.props.y.toString() + "dwad" + text.scrollWidth);
+            let text = j.children[1] as React.SVGProps<SVGTextElement>;
+            //text.
+        }
+        
+        //console.log(text.clientWidth);
+        //if (text is )
+    }
+    public render() {
+        return (
+            <g onDoubleClick={() => this.toggleEditMode()} id={"cell:" + this.props.xpixel.toString() + this.props.ypixel.toString()} ref={this.ref}>
+                <rect x={this.props.xpixel} y={this.props.ypixel} width={this.props.width} height={this.props.height} fill="grey" />
+                {this.getText()}
+            </g>
         );
     }
 }
